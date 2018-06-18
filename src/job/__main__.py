21fb1f18ecc3805095c1d3cc8a552d5224e0884c
oftl <oftl@rockandroll.at>
2018-06-18 13:56:24 +0200

    ensure configfile exists

--- src/job/__main__.py
+++ src/job/__main__.py
@@ -4,6 +4,8 @@ import yaml
 from job.main import Job
 
 configfile = '%s/job.yml' % os.path.dirname (os.path.realpath (__file__))
+if not os.path.isfile (configfile):
+    raise LookupError ('missing config file <%s>' % configfile)
 
 with open (configfile, 'r') as config:
     job = Job (cfg = yaml.load (config))
